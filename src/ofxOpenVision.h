0bd8527a5a61054d8a2e6522b15ecdfc07e0755c
Tobias Ebsen <tobiasebsen@gmail.com>
2012-09-13 15:33:37 +0200

    Added shader dir

--- src/ofxOpenVision.h
+++ src/ofxOpenVision.h
@@ -15,18 +15,29 @@ class ovFilter : public ofFbo {
 public:
 	
 	virtual void allocate(int width, int height) = 0;
+
 	virtual ofTexture& apply(ofTexture& texture) = 0;
+	
+	ofShader& getShader() {
+		return shader;
+	}
 
 protected:
 
 	void allocate(int width, int height, string shaderFile, int internalFormat = GL_RGB, bool useArbTex = true) {
+
+		// Load shader
 		shader.load("", shaderFile);
+		
+		// Allocate framebuffer object
 		ofFbo::Settings settings;
 		settings.width = width;
 		settings.height = height;
 		settings.internalformat = internalFormat;
 		settings.textureTarget = useArbTex ? GL_TEXTURE_RECTANGLE_ARB : GL_TEXTURE_2D;
 		ofFbo::allocate(settings);
+		
+		// Clear the framebuffer object
 		ofFbo::begin();
 		ofClear(0, 0, 0, 255);
 		ofFbo::end();
